431cef088397977db986e6c98698b0bbb139b0a0
Motasm Abdelmajed <motasmabdelmajed@gmail.com>
2018-10-28 16:35:01 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+/*
+var age = prompt("how old are you");
+
+
+if (age >= 18) {  //Annars     detta är sant
+    alert("old enough");  // Öppnar sidan.  //denna raden kallas condition statement
+
+
+}
+else if (age < 18) { //Annars om      detta är falskt
+    alert("sorry! old not enough");
+    open(""); //öppnar en blank sida
+
+
+
+}
+else {
+    alert("Whaaaat!?"); // om du skriver med bokstäver så dyker denna upp. detta är varken sant eller falskt
+    open("");
+}
+
+
+
+
+var name = prompt("what's your name?");
+if (name === "motasm") {
+    alert("Welcome");
+}
+
+else {
+    alert("Byeee");
+    open("");
+}
+*/
+
+

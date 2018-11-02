fcead8e51aba0ab8f16473f5a8645bd869dab42a
Motasm Abdelmajed <motasmabdelmajed@gmail.com>
2018-11-02 15:30:44 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+/*
+
+var agee = prompt("how old are you?")
+
+var age = prompt("how old are you")
+if (age >=18) {  
+    alert("old enough")
+}else if (age < 18) {
+    alert("sorry not old enough");
+    open("")
+ } else {
+    alert("Whaaaat!?");  falskt
+    
+};
+var name = prompt("What's your name??");
+if (name === "Junior") {
+    alert("Enjoy!!!!");
+}else {
+    alert("hejdå");
+    open("");
+};
+ */
+
